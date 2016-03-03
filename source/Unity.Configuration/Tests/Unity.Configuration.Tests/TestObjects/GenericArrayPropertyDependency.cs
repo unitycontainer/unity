bff423458d0fa98b823aef8a8f85364762b74758
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-03-03 15:24:55 -0300

    remove old projects

--- source/Unity.Configuration/Tests/Unity.Configuration.Tests/TestObjects/GenericArrayPropertyDependency.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
-
-namespace Microsoft.Practices.Unity.Configuration.Tests.TestObjects
-{
-    internal class GenericArrayPropertyDependency<T>
-    {
-        public T[] Stuff { get; set; }
-    }
-}

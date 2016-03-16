6bfc9aae95d6d23e8cd88fdf18c062fca4813791
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-03-16 15:05:43 -0300

    add targets

--- source/Src/Unity-CoreClr/ObjectBuilder/Exceptions/IllegalInjectionMethodException.cs
+++ source/Src/Unity-CoreClr/ObjectBuilder/Exceptions/IllegalInjectionMethodException.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
 using System;
-using System.Runtime.Serialization;
 
 namespace ObjectBuilder2
 {

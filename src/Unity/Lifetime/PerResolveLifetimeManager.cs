22db27b7fcb9f3157a211ee57c0a7236a689c7d7
Eugene Sadovoi <evgeni@eniks.com>
2017-09-19 11:55:36 -0400

    Update PerResolveLifetimeManager.cs

--- src/Unity/Lifetime/PerResolveLifetimeManager.cs
+++ src/Unity/Lifetime/PerResolveLifetimeManager.cs
@@ -1,4 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+using System.Diagnostics;
+
 
 namespace Unity
 {
@@ -9,28 +11,10 @@ namespace Unity
     /// </summary>
     public class PerResolveLifetimeManager : LifetimeManager
     {
-        private readonly object value;
+        // TODO: Use InUse instead
+        private bool isSet;
 
-        /// <summary>
-        /// Construct a new <see cref="PerResolveLifetimeManager"/> object that does not
-        /// itself manage an instance.
-        /// </summary>
-        public PerResolveLifetimeManager()
-        {
-        }
-
-        /// <summary>
-        /// Construct a new <see cref="PerResolveLifetimeManager"/> object that stores the
-        /// give value. This value will be returned by <see cref="LifetimeManager.GetValue"/>
-        /// but is not stored in the lifetime manager, nor is the value disposed.
-        /// This Lifetime manager is intended only for internal use, which is why the
-        /// normal <see cref="LifetimeManager.SetValue"/> method is not used here.
-        /// </summary>
-        /// <param name="value">Value to store.</param>
-        internal PerResolveLifetimeManager(object value)
-        {
-            this.value = value;
-        }
+        private object value;
 
         /// <summary>
         /// Retrieve a value from the backing store associated with this Lifetime policy.
@@ -38,7 +22,7 @@ namespace Unity
         /// <returns>the object desired, or null if no such object is currently stored.</returns>
         public override object GetValue()
         {
-            return this.value;
+            return value;
         }
 
         /// <summary>
@@ -48,6 +32,10 @@ namespace Unity
         /// <param name="newValue">The object being stored.</param>
         public override void SetValue(object newValue)
         {
+            Debug.Assert(!isSet, "The value for PerResolveLifetimeManager can only be set once");
+
+            isSet = true;
+            value = newValue;
         }
 
         /// <summary>

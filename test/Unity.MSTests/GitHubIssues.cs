30fd98fa1842934ae561713a86092e3fa245c34b
Eugene Sadovoi <evgeni@eniks.com>
2017-09-21 15:15:39 -0400

    Create GitHubIssues.cs

---
+++ test/Unity.MSTests/GitHubIssues.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+
+namespace GitHub
+{
+    // https://github.com/unitycontainer/unity/issues/54
+    [TestClass]
+    public class Issues
+    {
+        // http://www.codeplex.com/unity/WorkItem/View.aspx?WorkItemId=1307
+        [TestMethod]
+        public void StackOverflowException_54()
+        {
+        }
+    }
+}
